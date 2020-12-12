78bd6f6e3d5d81aa3321b850493ca056b0663803
Rafael Oliveira <raaf.forsoft.25@gmail.com>
2020-12-12 14:10:25 -0300

    install unform lib

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Form } from '@unform/web';
 
 import {
   FiMail,
@@ -18,25 +19,31 @@ import logoImg from '../../assets/logo.svg';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 
-const SignUp: React.FC = () => (
-  <Container>
-    <Background />
-    <Content>
-      <img src={logoImg} alt="GoBarber" />
-      <form>
-        <h1>Faça o seu Cadastro</h1>
-        <Input icon={FiUser} name="nome" placeholder="Nome" />
-        <Input icon={FiMail} name="email" placeholder="E-mail" />
-        <Input icon={FiLock} name="password" type="password" placeholder="Senha" />
-        <Button type="submit">Cadastrar</Button>
-      </form>
+const SignUp: React.FC = () => {
 
-      <a href="signup">
-        <FiArrowLeft />
-        Voltar para o logon
-      </a>
-    </Content>
-  </Container>
-);
+  function handleSubmit(data: object): void {
+    console.log(data);
+  }
+  return (
+    <Container>
+      <Background />
+      <Content>
+        <img src={logoImg} alt="GoBarber" />
+        <Form onSubmit={handleSubmit}>
+          <h1>Faça o seu Cadastro</h1>
+          <Input icon={FiUser} name="nome" placeholder="Nome" />
+          <Input icon={FiMail} name="email" placeholder="E-mail" />
+          <Input icon={FiLock} name="password" type="password" placeholder="Senha" />
+          <Button type="submit">Cadastrar</Button>
+        </Form>
+
+        <a href="signup">
+          <FiArrowLeft />
+          Voltar para o logon
+        </a>
+      </Content>
+    </Container>
+  );
+}
 
 export default SignUp;
